5acf230427531597ba7130d9b563eb2a0d0d22ee
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-08 19:40:49 +0530

    Update single_dim_array.sv

--- Arrays /Packed_array /single_dim_array.sv
+++ Arrays /Packed_array /single_dim_array.sv
@@ -1,19 +1,21 @@
-module tb;
-  bit [7:0] data;
-  initial 
+module array_tb;
+  bit [7:0] arr;
+  initial
     begin
-      data=8'h45;
-      for(int i=0;i<7;i++)begin
-        $display("data[%od]=%b",i,data[i]);
-      end
+      arr = 8'h22;
+      for(int i=0;i<$size(arr);i++)
+        begin
+              $display("arr[%0d]=%b",i,arr[i]);
+        end
     end
 endmodule
 
-//output
-//data[00000000000d]=1
-//data[00000000001d]=0
-//data[00000000002d]=1
-//data[00000000003d]=0
-//data[00000000004d]=0
-//data[00000000005d]=0
-//data[00000000006d]=1
+/*output
+arr[0]=0
+arr[1]=1
+arr[2]=0
+arr[3]=0
+arr[4]=0
+arr[5]=1
+arr[6]=0
+arr[7]=0*/
