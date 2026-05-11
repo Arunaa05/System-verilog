ae5c635550177618810e431c561b763e2e691ebe
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-11 12:21:02 +0530

    Create 1.Class.sv

---
+++ Class /1.Class.sv
@@ -0,0 +1,24 @@
+class add;
+  int a, b;
+  function int ADD(); // functions or task are called as class methods
+    ADD=a+b;
+  endfunction
+  
+endclass
+
+module demo;
+  
+  initial begin
+  add c = new();
+  c.a = 5;
+  c.b =5;
+    $display("ADD = %0d",c.ADD());
+  end
+  
+endmodule
+
+/* output
+xcelium> run
+ADD = 10
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit */
