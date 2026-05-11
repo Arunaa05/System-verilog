e5362ca671426d546c30006d68b99511acbe8258
Aruna Annadurai <arunaparimala76@gmail.com>
2026-05-11 13:14:38 +0530

    Create 3.Class_handle.sv

---
+++ Class /3.Class_handle.sv
@@ -0,0 +1,23 @@
+class Packet;
+	int count;
+endclass
+
+module tb;
+  	
+	Packet pkt,pkt1;
+  initial begin
+      pkt = new();
+      pkt.count=4'hf;
+    $display ("count=%0d",pkt.count);
+    pkt1=pkt;
+    $display ("count = %0d", pkt1.count);
+  	end
+endmodule
+
+/* output 
+xcelium> run
+count=15
+count = 15
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
+*/
