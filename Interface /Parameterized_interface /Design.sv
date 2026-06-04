5481f9acf5187b514034c3e8d91f421ece3e1ec4
Aruna Annadurai <arunaparimala76@gmail.com>
2026-06-04 12:14:07 +0530

    Create Design.sv

---
+++ Interface /Parameterized_interface /Design.sv
@@ -0,0 +1,16 @@
+module up_counter(clk,reset,counter);
+  
+  input clk, reset;
+  output [2:0] counter;
+  reg [2:0] counter_up;
+  
+  always @(posedge clk or posedge reset) begin
+
+    if(reset)
+      counter_up <= 3'b0;
+    else
+      counter_up <= counter_up + 3'b1;
+  end
+  
+  assign counter = counter_up;
+endmodule:up_counter
