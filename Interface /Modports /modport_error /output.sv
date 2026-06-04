50e562ed6aa89cbfa22e0777dc50693446b5b31e
Aruna Annadurai <arunaparimala76@gmail.com>
2026-06-04 11:22:08 +0530

    Create output.sv

---
+++ Interface /Modports /modport_error /output.sv
@@ -0,0 +1,11 @@
+xrun: 25.03-s001: (c) Copyright 1995-2025 Cadence Design Systems, Inc.
+
+	Top level design units:
+		top
+  assign vif.dut_MP.r=(vif.dut_MP.p)&(vif.dut_MP.q);
+                                  |
+xmelab: *E,CUVUNF (./design.sv,4|34): Hierarchical name component lookup failed for 'p' at 'top.a1'.
+
+xrun: *E,ELBERR: Error (*E) or soft error (*SE) occurred during elaboration (status 1), exiting.
+TOOL:	xrun	25.03-s001: Exiting on Jun 04, 2026 at 01:46:21 EDT  (total: 00:00:00)
+Exit code expected: 0, received: 1
