f24414667900f3613673f73d26e2bcc3c4dbdff6
Aruna Annadurai <arunaparimala76@gmail.com>
2026-06-10 23:55:32 +0530

    Create fork_join_none.sv

---
+++ Processes /fork_join_none.sv
@@ -0,0 +1,32 @@
+module fork_join_none; 
+  
+  initial begin
+    
+    $display("time =%0t ,fork Block Begins",$stime);
+    fork 
+      $display("time =%0t , Thread_1 completed ",$stime);
+      #6 $display("time =%0t , Thread_2 completed",$stime);
+      #5 $display("time =%0t , Thread_3 completed",$stime);
+      #2 $display("time =%0t , Thread_4 completed",$stime);
+    join_none
+    #3;
+    $display("time = %0t , Join block executed",$stime);
+    #1 $display("time = %0t without fork_join",$time);
+    #1 $display("time = %0t Task_1 done",$time);
+    #3 $display("time = %0t Task_2 done",$time);
+    
+  end
+    
+endmodule
+
+/*output
+time =0 ,fork Block Begins
+time =0 , Thread_1 completed 
+time =2 , Thread_4 completed
+time = 3 , Join block executed
+time = 4 without fork_join
+time =5 , Thread_3 completed
+time = 5 Task_1 done
+time =6 , Thread_2 completed
+time = 8 Task_2 done
+*/
